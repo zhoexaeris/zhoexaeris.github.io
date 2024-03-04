2c889a061bf9d39b2075657029c78a17455e01a0
zhoexaeris <zhoe_gongon@dlsu.edu.ph>
2024-03-04 23:08:56 +0800

    added landing header

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+"use client";
+import React, {useState} from 'react'
+import { TypeAnimation } from 'react-type-animation';
+
+const Tagline = () => {
+    return (
+        <span className='text-3xl font-medium'>
+        <TypeAnimation
+      sequence={[
+        'Aspiring UI/UX designer',
+        200,
+        'Aspiring front-end developer',
+        200,
+        'And a dog lover', 
+        200,
+        () => {
+          console.log('Sequence completed');
+        },
+      ]}
+      cursor={true}
+      repeat={Infinity}
+      />
+      </span>
+    );
+  };
 
 const HeroSection = () => {
   return (
-    <section className='text-black'>
-        <p>This is the hero section</p>
-        <p>Lorem ipsum dolor sit amet</p>
+    <section className='text-black bg-light-green h-96 px-4'>
+        <div className='flex h-full items-center justify-center px-4'>
+            <div className='flex-row'>
+            <h1 className='text-6xl font-extrabold'>
+                Hi, I am Zhoe Aeris.
+            </h1>
+            <p className='text-3xl font-medium'> 
+                <Tagline />
+            </p>
+            </div>
+            
+            
+        </div>
+        
+        
+        
     </section>
   )
 }
